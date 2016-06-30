8ecc43576f60fb188afc9efc8ef5c474ad1363a1
MateuszsuetaM <matthewcfc@gmail.com>
2016-06-30 08:01:32 +0200

    Polaczenie z baza danych mysql

---
+++ cpp_mysql/pierwsze_polaczenie.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <mysql/mysql.h>
+using namespace std;
+int main() {
+
+  MYSQL mysql;
+  mysql_init(&mysql);
+  if (mysql_real_connect(&mysql, "127.0.0.1", "root", "admin1", "pierwsza", 0,
+                         NULL, 0))
+    cout << "Nawiazano polaczenie" << endl;
+  else
+    cout << "Niestety nie nawiazano polaczenia" << endl;
+
+  MYSQL_RES *idZapytania;
+  MYSQL_ROW wiersz;
+
+  mysql_select_db(&mysql, "pierwsza");
+  mysql_query(&mysql, "SELECT * FROM dane");
+  idZapytania = mysql_store_result(&mysql);
+  // MYSQL_ROW wiersz = mysql_fetch_row(idZapytania);
+  // cout << wiersz[2] << endl;
+  // cout << wiersz[1] << endl;
+  // cout << wiersz[2] << endl;
+
+  while ((wiersz = mysql_fetch_row(idZapytania)) != NULL) {
+    for (int i = 0; i < mysql_num_fields(idZapytania); i++)
+      // for (int i = 0; i < 4; i++)
+      cout << wiersz[i] << " ";
+
+    cout << endl;
+  }
+
+  mysql_close(&mysql);
+}
